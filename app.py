de2fb9fe82384db2179c021e4f5025fba8e2e8c4
luna0228 <51066795+luna0228@users.noreply.github.com>
2023-11-20 15:07:13 +0800

    update fastapi with python

---
+++ app.py
@@ -0,0 +1,27 @@
+import uvicorn
+from fastapi import FastAPI
+from homework import homework
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return homework
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", port=5000, reload=True)
+
+origins = [
+    'http://localhost:5173',
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=['*']
+)
